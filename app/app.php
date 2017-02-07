c9e9c8a2dab2321f6fc27470daf16293c1413bad
Patrick McGreevy and Zach Swanson <zach@>
2017-02-07 08:38:09 -0800

    Initial commit

---
+++ app/app.php
@@ -0,0 +1,13 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+
+    require_once __DIR__ . '/../vendor/autoload.php';
+
+    $app = new Silex\Application();
+
+    $app->get('/', function() {
+        return 'Sample root page';
+    });
+
+    return $app;
+?>
